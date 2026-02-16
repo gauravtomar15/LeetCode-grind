0bc5cc6993c340b0c86cd4554583e0d0800510b7
Gaurav Tomar <gauravtomar977@gmail.com>
2026-02-16 14:12:11 +0530

    Time: 0 ms (100%), Space: 95.2 MB (62.73%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,35 +3,63 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size();
         int n = nums2.size();
-        vector<int> temp(m+n);
+        int size = m+n;
+
+        int idx1 = (size/2)-1;
+        int element1 = -1;
+
+        int idx2 = (size/2);
+        int element2 = -1;
 
         int i =0 , j=0, k=0;
         while(i<m && j<n){
             if(nums1[i]<nums2[j]){
-              temp[k] = nums1[i];
+              if(k==idx1){
+                element1 = nums1[i];
+              }
+              if(k==idx2){
+                element2 = nums1[i];
+              }
               i++;
-              k++;
             }
             else{
-                temp[k] = nums2[j];
+                if(k==idx1){
+                    element1 = nums2[j];
+                }
+                if(k==idx2){
+                    element2 = nums2[j];
+                }
                 j++;
-                k++;
             }
+            k++;
         }
         while(i<m){
-            temp[k] = nums1[i];
-            i++;
-            k++;
+             if(k==idx1){
+                element1 = nums1[i];
+              }
+              if(k==idx2){
+                element2 = nums1[i];
+              }
+              i++;
+              k++;
+          
         }
          while(j<n){
-            temp[k] = nums2[j];
-            j++;
-            k++;
+              if(k==idx1){
+                    element1 = nums2[j];
+
+                }
+                if(k==idx2){
+                    element2 = nums2[j];
+                }
+                j++;
+                k++;
+           
         }
-        int size = m+n;
+    
         if(size%2 ==1){
-            return temp[size/2];
+            return element2;
         }
-        return (temp[size/2] + temp[(size/2) -1])/2.0;
+        return (element1 + element2)/2.0;
     }
 };
