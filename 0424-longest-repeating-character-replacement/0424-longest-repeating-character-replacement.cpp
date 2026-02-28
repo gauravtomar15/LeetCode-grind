ce16b21bbb0758b660ef94c50bd91c0e3e53343a
Gaurav Tomar <gauravtomar977@gmail.com>
2026-02-28 13:26:09 +0530

    Time: 26 ms (8.97%), Space: 10.9 MB (39.96%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
+    int solve(vector<int>& arr){
+        int ans =0;
+        for(int i =0; i<arr.size();i++){
+            ans = max(ans, arr[i]); 
+        }
+        return ans;
+    }
     int characterReplacement(string s, int k) {
-        vector<int> arr(256);
+        vector<int> arr(26);
         int low = 0;
         int high = 0;
-        int res = INT_MIN;
+        int res = -1;
+        
+
         for (high = 0; high < s.size(); high++) {
-            arr[s[high]]++;
+            arr[s[high] - 'A']++;
             int len = high - low + 1;
-            int maxCount = max(maxCount, arr[s[high]]);
+            int maxCount = solve(arr);
             int diff = len - maxCount;
+
             while (diff > k) {
-                arr[s[low]]--;
+                arr[s[low]-'A']--;
                 low++;
                 len = high - low + 1;
-                maxCount = max(maxCount, arr[s[high]]);
+                maxCount = solve(arr);
                 diff = len - maxCount;
             }
-            len = high - low + 1;
-            res = max(res, len);
+        
+
+           res = max(res, high - low + 1);
         }
+
         return res;
     }
 };
