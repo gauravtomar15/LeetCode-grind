9fe1cd6bc80db290c7ce435675e924371398a82e
Gaurav Tomar <gauravtomar977@gmail.com>
2026-03-01 16:52:22 +0530

    Time: 0 ms (100%), Space: 132.1 MB (14.36%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -9,32 +9,26 @@
  * };
  */
 class Solution {
-private : 
-    bool checkPalindrom(vector <int> arr){
-        int n = arr.size();
-        int s = 0;
-        int e = n-1;
-
-        while(s<=e){
-            if(arr[s]!=arr[e]){
-                return 0;
-            }
-            s++;
-            e--;
-            
-        }
-        return 1;
-
-    }     
 public:
+    bool checkPalindrome(vector<int>& arr) {
+        int n = arr.size();
+        int start = 0, end = n - 1;
+        while (start <= end) {
+            if (arr[start] != arr[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
     bool isPalindrome(ListNode* head) {
         vector<int> arr;
         ListNode* temp = head;
-        while(temp != NULL){
+        while (temp!=NULL) {
             arr.push_back(temp->val);
             temp = temp->next;
         }
-        return checkPalindrom(arr);
-        
+        return checkPalindrome(arr);
     }
 };
