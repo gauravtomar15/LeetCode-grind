d514d68d7ae9e6215807adc8916ed88dff93e54d
Gaurav Tomar <gauravtomar977@gmail.com>
2026-03-01 16:14:34 +0530

    Time: 8 ms (47.35%), Space: 65.1 MB (40.44%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-
-        int slow = nums[0];  // ye slow pointer ye one step chalega
-        int fast = nums[0];  // ye faster fointer jo slow pointer k twice chalega
-        slow = nums[slow];
-        fast = nums[nums[fast]];
-
-        // Detect  cycle technique name (hare & tortoise) ye floyd algorithm follow krta hai
-        while(slow != fast){
+        int slow =nums[0];
+        int fast =nums[0];
+        while(true){
             slow = nums[slow];
             fast = nums[nums[fast]];
+            if(slow == fast){
+                slow = nums[0];
+                while(slow!=fast){
+                    slow = nums[slow];
+                    fast = nums[fast];
+                }
+                return slow;
+            }
         }
-        slow= nums[0];
-        
-        // ab hume pata chal gya ha ki cycle kha ban rhi ha than ab hum apne
-        // slow ko start pr target krenge or fast ko whi rehne denge or jaha slow || fast milenge whi deuplicate hoga 
-        while(slow!=fast){
-            slow = nums[slow];
-            fast = nums[fast];
-        }
-        return slow;
-        
+        return -1;
     }
 };
