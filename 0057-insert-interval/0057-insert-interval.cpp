946ddd4639b9732092cc1f1a4feac38e37b4715a
Gaurav Tomar <gauravtomar977@gmail.com>
2026-03-18 13:30:02 +0530

    Time: 2 ms (33.05%), Space: 23.2 MB (5.94%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -2,6 +2,27 @@ class Solution {
     static bool sortCol(const vector<int>& v1, const vector<int>& v2) {
         return v1[0] < v2[0];
     }
+    vector<vector<int>> merge(vector<vector<int>>& res) {
+        int n = res.size();
+        vector<vector<int>> res2;
+
+        int start1 = res[0][0];
+        int end1 = res[0][1];
+        for (int i = 1; i < n; i++) {
+            int start2 = res[i][0];
+            int end2 = res[i][1];
+            if (end1 >= start2) {
+                start1 = start1;
+                end1 = max(end1, end2);
+                continue;
+            }
+            res2.push_back({start1, end1});
+            start1 = start2;
+            end1 = end2;
+        }
+        res2.push_back({start1, end1});
+        return res2;
+    }
 
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals,
@@ -21,23 +42,7 @@ public:
         }
 
         sort(res.begin(), res.end(), sortCol);
-        vector<vector<int>> res2;
 
-        int start1 = res[0][0];
-        int end1 = res[0][1];
-        for (int i = 1; i < n+1; i++) {
-            int start2 = res[i][0];
-            int end2 = res[i][1];
-            if (end1 >= start2) {
-                start1 = start1;
-                end1 = max(end1, end2);
-                continue;
-            }
-            res2.push_back({start1, end1});
-            start1 = start2;
-            end1 = end2;
-        }
-        res2.push_back({start1, end1});
-        return res2;
+        return merge(res);
     }
 };
