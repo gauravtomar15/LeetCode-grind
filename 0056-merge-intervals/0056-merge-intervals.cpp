98f2dce1e7d0ec73aa1423a11f187b7e44811bea
Gaurav Tomar <gauravtomar977@gmail.com>
2026-03-19 21:59:09 +0530

    Time: 3 ms (89.41%), Space: 23.9 MB (59.07%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,15 +6,17 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n = intervals.size();
-        vector<vector<int>> res;
         sort(intervals.begin(), intervals.end(), sortCol);
         int start1 = intervals[0][0];
         int end1 = intervals[0][1];
 
+        vector<vector<int>> res;
+
         for (int i = 1; i < n; i++) {
             int start2 = intervals[i][0];
             int end2 = intervals[i][1];
-            if (end1 >= start2) { // merge krna ha
+
+            if (end1 >= start2) {
                 start1 = start1;
                 end1 = max(end1, end2);
                 continue;
