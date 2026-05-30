cc447f0981a3297c1998361d83158e0370882ead
Gaurav Tomar <gauravtomar977@gmail.com>
2026-05-30 13:34:41 +0530

    Time: 50 ms (72.41%), Space: 18.6 MB (92.62%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,31 +1,46 @@
 class Solution {
 public:
-
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();  // Total number of rows
-        int n = matrix[0].size();     // Total number of columns
-        
-        int row = 0;   // Start from first row
-        int col = n - 1;     // Start from last column (top-right corner)
-        
-        // Continue until row and column remain inside matrix
-        while (row < m && col >= 0) {
-            // If current element equals target
+        if (matrix.empty() || matrix[0].empty()) {
+            return false;
+        }
+        int m = matrix.size();    // Total number of rows
+        int n = matrix[0].size(); // Total number of columns
+        int row = m - 1;
+        int col = 0;
+        while (row >= 0 && col < n) {
             if (matrix[row][col] == target) {
                 return true;
-            }
-            // If current element is greater than target
-            // move left because all elements below are bigger
-            else if (matrix[row][col] > target) {
-                col--;
-            }
-            // If current element is smaller than target
-            // move downward because all elements left are smaller
-            else {
-                row++;
+            } else if (matrix[row][col] < target) {
+                col++;
+            } else if (matrix[row][col] > target) {
+                row--;
             }
         }
-        // Target not found
+
         return false;
+
+        // int row = 0;   // Start from first row
+        // int col = n - 1;     // Start from last column (top-right corner)
+
+        // // Continue until row and column remain inside matrix
+        // while (row < m && col >= 0) {
+        //     // If current element equals target
+        //     if (matrix[row][col] == target) {
+        //         return true;
+        //     }
+        //     // If current element is greater than target
+        //     // move left because all elements below are bigger
+        //     else if (matrix[row][col] > target) {
+        //         col--;
+        //     }
+        //     // If current element is smaller than target
+        //     // move downward because all elements left are smaller
+        //     else {
+        //         row++;
+        //     }
+        // }
+        // // Target not found
+        // return false;
     }
 };
