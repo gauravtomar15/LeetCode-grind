0df18c1410817899bd856d93a6e71636c27d1abc
Gaurav Tomar <gauravtomar977@gmail.com>
2026-06-17 11:21:57 +0530

    Time: 77 ms (79.69%), Space: 44.3 MB (81.32%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -2,38 +2,37 @@ class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<vector<pair<int, int>>> adj(n);
+        vector<int> dist(n, INT_MAX);
         for (int i = 0; i < times.size(); i++) {
             int source = times[i][0];
             int distance = times[i][1];
             int weight = times[i][2];
             adj[source - 1].push_back({distance - 1, weight});
         }
-        priority_queue<pair<int, int>, vector<pair<int, int>>,
-                       greater<pair<int, int>>>
-            pq;
-        vector<int> dist(n, INT_MAX);
-        dist[k - 1] = 0;
+        using p = pair<int, int>;
+        priority_queue<p, vector<p>, greater<p>> pq;
         pq.push({0, k - 1});
+        dist[k - 1] = 0;
+
         while (!pq.empty()) {
             pair<int, int> t = pq.top();
             pq.pop();
-            int d = t.first;
             int node = t.second;
-            if (d > dist[node]) {
+            int wt = t.first;
+            if (wt > dist[node])
                 continue;
-            }
             for (int i = 0; i < adj[node].size(); i++) {
                 int neigh = adj[node][i].first;
-                int wt = adj[node][i].second;
-                if (d + wt < dist[neigh]) {
-                    dist[neigh] = d + wt;
-                    pq.push({d + wt, neigh});
+                int newWt = adj[node][i].second;
+                if (newWt + wt < dist[neigh]) {
+                    dist[neigh] = newWt + wt;
+                    pq.push({newWt + wt, neigh});
                 }
             }
         }
         sort(dist.begin(), dist.end());
         int n1 = dist.size();
-        if (dist[n1 - 1] == INT_MAX) {
+        if (dist[n - 1] == INT_MAX) {
             return -1;
         }
         return dist[n1 - 1];
