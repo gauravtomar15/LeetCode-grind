61bdf3de81935fdb62b5102b066fa637e66f3050
Gaurav Tomar <gauravtomar977@gmail.com>
2026-06-21 12:47:40 +0530

    Time: 0 ms (100%), Space: 15.8 MB (32.48%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        
         int n = nums.size();
         int target_idx = -1;
 
-        // target index find karenge pehle
         for (int i = n - 1; i > 0; i--) {
-            if (nums[i] > nums[i - 1]) {
+            if (nums[i - 1] < nums[i]) {
                 target_idx = i - 1;
                 break;
             }
         }
+        // swap krne ke liye find kro
 
-        // swap krne ke liye index dhundnge kha se krna ha h swap
         if (target_idx != -1) {
             int swap_idx = target_idx;
             for (int j = n - 1; j >= target_idx + 1; j--) {
+
                 if (nums[target_idx] < nums[j]) {
                     swap_idx = j;
                     break;
