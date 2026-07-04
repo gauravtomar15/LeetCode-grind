bf299a461f41994c46b200d554ce539831f91809
Gaurav Tomar <gauravtomar977@gmail.com>
2026-07-05 00:50:40 +0530

    Time: 23 ms (54.5%), Space: 17.3 MB (84.15%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-         vector<vector<int>> ans;
-         int n = nums.size();
-
-         sort(nums.begin() , nums.end());
-
-         for(int i = 0; i<n; i++){
-            if(i>0 && nums[i]==nums[i-1])  continue; // agar hum ith index ka elementsame milta ha to hum usko skip kr denge or i++ ho jayega 
-            for(int j=i+1; j<n;){
-                
-                int p = j+1 , q = n-1;
-                while(p<q){
-                    long long sum = (long long)nums[i] +(long long)nums[j]+ (long long)nums[p] + (long long)nums[q];
-                    if(sum>target){
+        int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            for (int j = i + 1; j < n;) {
+                int p = j + 1;
+                int q = n - 1;
+                while (p < q) {
+                    long long sum = (long long)nums[i] + (long long)nums[j] +
+                                    (long long)nums[p] + (long long)nums[q];
+                    if (sum < target) {
+                        p++;
+                    } else if (sum > target) {
                         q--;
-                    } else if(sum<target){
+                    } else {
+                        ans.push_back({nums[i], nums[j], nums[p], nums[q]});
                         p++;
-                    } else {  // sum==target
-                        ans.push_back({nums[i],nums[j],nums[p],nums[q]});
-                        p++;
-                        q--;  
-                        while(p<q && nums[p]==nums[p-1]) p++; // same as work i and j
+                        q--;
+                        while (p < q && nums[p] == nums[p - 1])
+                            p++;
+                        while (p < q && nums[q] == nums[q + 1])
+                            q--;
                     }
                 }
                 j++;
-                while(j<n && nums[j]==nums[j-1]) j++; // yha  bhi same j ke liye ha jab bhi same element milega usko aage bada denge
+                while(j < n && nums[j] == nums[j - 1])
+                    j++;
             }
-         }
-         return ans;
+        }
+        return ans;
     }
 };
