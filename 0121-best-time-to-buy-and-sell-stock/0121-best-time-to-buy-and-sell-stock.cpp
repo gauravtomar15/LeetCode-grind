f95d72ac618cca2a070792cd21a44d36a75139cf
Gaurav Tomar <gauravtomar977@gmail.com>
2026-07-12 12:06:08 +0530

    Time: 362 ms (6.4%), Space: 206.7 MB (5.26%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,36 @@
 class Solution {
 public:
+    int func(vector<int>& prices, int n, int i, int k,
+             vector<vector<int>>& dp) {
+        if (i == n) {
+            return 0;
+        }
+        if (k == 0) {
+            return 0;
+        }
+        if (dp[i][k] != -1) {
+            return dp[i][k];
+        }
+        if (k == 2) {
+            int c1 = func(prices, n, i + 1, k - 1, dp) - prices[i];
+            int c2 = func(prices, n, i + 1, k, dp);
+            return dp[i][k] = max(c1, c2);
+        } else {
+            int c1 = func(prices, n, i + 1, k - 1, dp) + prices[i];
+            int c2 = func(prices, n, i + 1, k, dp);
+            return dp[i][k] = max(c1, c2);
+        }
+        return 0;
+    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        int mini = prices[0];
-        int profit = 0;
-
-        for (int i = 1; i < n; i++) {
-            int diff = prices[i] - mini;
-            profit = max(profit, diff);
-            mini = min(mini, prices[i]);
+        int k = 2;
+        vector<vector<int>> dp(n + 1);
+        for (int i = 0; i < n; i++) {
+            vector<int> temp(k + 1, -1);
+            dp[i] = temp;
         }
-        return profit;
+        int gain = func(prices, n, 0, k, dp);
+        return gain;
     }
 };
