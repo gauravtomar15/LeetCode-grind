cb2d6e4e5672cdfd07c361e8dee228d7a25f7a34
Gaurav Tomar <gauravtomar977@gmail.com>
2026-07-28 10:47:23 +0530

    Time: 0 ms (100%), Space: 27.1 MB (95.94%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     int maximumSum(vector<int>& arr) {
-        int noDelete = arr[0];
-        int oneDelete = INT_MIN; 
-        int res  = arr[0];
+        int bestEndingNoDelete = arr[0];
+        int bestEndingOneDelete = 0;
+        int result = arr[0];
 
         for (int i = 1; i < arr.size(); i++) {
-            int prevNoDelete = noDelete;
-            int prevOneDelete = oneDelete;
+            int prevNoDelete = bestEndingNoDelete;
+            int prevOneDelete = bestEndingOneDelete;
+            bestEndingNoDelete = max(prevNoDelete + arr[i], arr[i]);
+            bestEndingOneDelete = max(prevOneDelete + arr[i], prevNoDelete);
 
-            noDelete = max(prevNoDelete + arr[i], arr[i]);
-
-            int v1;
-            if (prevOneDelete == INT_MIN) {
-                v1 = arr[i];
-            } else {
-                v1 = prevOneDelete + arr[i];
-            }
-            oneDelete = max(v1, prevNoDelete);
-            res = max(res, max(oneDelete, noDelete));
+            result = max(result, max(bestEndingNoDelete, bestEndingOneDelete));
         }
-        return res;
+        return result;
     }
 };
