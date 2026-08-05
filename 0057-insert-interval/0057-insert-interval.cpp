cc4b42bf7b77f9cca6fd47918c477392f9dd7087
Gaurav Tomar <gauravtomar977@gmail.com>
2026-08-05 17:14:25 +0530

    Time: 0 ms (100%), Space: 23.2 MB (6.09%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,48 +1,42 @@
 class Solution {
-    static bool sortCol(const vector<int>& v1, const vector<int>& v2) {
-        return v1[0] < v2[0];
-    }
-    vector<vector<int>> merge(vector<vector<int>>& res) {
-        int n = res.size();
-        vector<vector<int>> res2;
-
-        int start1 = res[0][0];
-        int end1 = res[0][1];
+public:
+    static bool cmp(vector<int>& v1, vector<int>& v2) { return v1[0] < v2[0]; }
+    vector<vector<int>> merge(vector<vector<int>>& ans) {
+        int n = ans.size();
+        int s1 = ans[0][0];
+        int e1 = ans[0][1];
+        vector<vector<int>> res;
         for (int i = 1; i < n; i++) {
-            int start2 = res[i][0];
-            int end2 = res[i][1];
-            if (end1 >= start2) {
-                start1 = start1;
-                end1 = max(end1, end2);
+            int s2 = ans[i][0];
+            int e2 = ans[i][1];
+            if (e1 >= s2) {
+                s1 = s1;
+                e1 = max(e1, e2);
                 continue;
             }
-            res2.push_back({start1, end1});
-            start1 = start2;
-            end1 = end2;
+            res.push_back({s1, e1});
+            s1 = s2;
+            e1 = e2;
         }
-        res2.push_back({start1, end1});
-        return res2;
+        res.push_back({s1, e1});
+        return res;
     }
-
-public:
     vector<vector<int>> insert(vector<vector<int>>& intervals,
                                vector<int>& newInterval) {
         int n = intervals.size();
-        bool add = false;
-        vector<vector<int>> res;
+        bool insertCheck = false;
+        vector<vector<int>> ans;
         for (int i = 0; i < n; i++) {
-            if (add == false && intervals[i][0] >= newInterval[0]) {
-                res.push_back(newInterval);
-                add = true;
+            if (insertCheck == false && intervals[i][0] >= newInterval[0]) {
+                ans.push_back(newInterval);
+                insertCheck = true;
             }
-            res.push_back(intervals[i]);
+            ans.push_back(intervals[i]);
         }
-        if (add == false) {
-            res.push_back(newInterval);
+        if (insertCheck == false) {
+            ans.push_back(newInterval);
         }
-
-        sort(res.begin(), res.end(), sortCol);
-
-        return merge(res);
+        sort(ans.begin(), ans.end(), cmp);
+        return merge(ans);
     }
 };
