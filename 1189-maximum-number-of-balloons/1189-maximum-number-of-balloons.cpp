18b0688525d408e505d3c02c8aa5a6341628c6d6
Gaurav Tomar <gauravtomar977@gmail.com>
2026-08-07 12:13:50 +0530

    Time: 3 ms (11.82%), Space: 9 MB (21.49%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -2,23 +2,24 @@ class Solution {
 public:
     int maxNumberOfBalloons(string text) {
         int n = text.size();
-        int res = INT_MAX;
-        unordered_map<char, int> have;
-        unordered_map<char, int> need;
+        unordered_map<char,int> need;
+        unordered_map<char,int> have;
         string s = "balloon";
-        for (int i = 0; i < s.size(); i++) {
+        int res = INT_MAX;
+        for(int i =0 ; i<s.size() ; i++){
             need[s[i]]++;
         }
-        for (int i = 0; i < n; i++) {
+        for(int i =0 ; i<n ; i++){
             have[text[i]]++;
         }
-        for (auto i : need) {
-            char c = i.first;
-            int fneed = i.second;
+        for(auto val : need){
+            char c = val.first;
+            int fneed =val.second;
             int fhave = have[c];
             int times = fhave/fneed;
-            res = min(res, times);
+            res = min(res , times);
         }
         return res;
+
     }
 };
