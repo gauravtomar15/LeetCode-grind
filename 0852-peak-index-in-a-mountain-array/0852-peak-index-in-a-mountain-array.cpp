0a03d9c5f02c3f381b6d243bad488f11c500d9cc
Gaurav Tomar <gauravtomar977@gmail.com>
2026-08-07 19:02:14 +0530

    Time: 0 ms (100%), Space: 63.6 MB (11.07%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size();
         int low = 0;
-        int high = arr.size()-1;
+        int high = n - 1;
         int res;
-        while(low<=high){
-            int mid =(low+high)/2;
-            if(arr[mid]<arr[mid+1]){
-                low=mid+1;
-            }
-            if(arr[mid]>arr[mid+1]){
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] < arr[mid + 1]) {
+                low = mid + 1;
+            } else if (arr[mid] > arr[mid + 1]) {
                 res = mid;
-                high = mid-1;
+                high = mid - 1;
             }
         }
         return res;
-        
     }
 };
