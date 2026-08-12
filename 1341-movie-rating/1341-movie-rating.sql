db5de08588e1045719cc3ffe6029488c20582212
Gaurav Tomar <gauravtomar977@gmail.com>
2026-08-12 22:53:23 +0530

    Time: 1349 ms (68.83%), Space: 0B (100%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,16 @@
+(SELECT name AS results
+FROM Users
+JOIN MovieRating USING(user_id)
+GROUP BY user_id
+ORDER BY COUNT(rating) DESC , name
+LIMIT 1)
+
+UNION ALL
+
+(SELECT title  AS results
+FROM Movies
+JOIN MovieRating USING(movie_id)
+WHERE MONTH(created_at) = '02' AND YEAR(created_at) = '2020'
+GROUP BY movie_id
+ORDER BY AVG(rating) DESC , title
+LIMIT 1)
