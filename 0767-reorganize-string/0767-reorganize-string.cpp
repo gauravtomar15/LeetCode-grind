b558a839e7d9edab45f206f8bef1d4f7cc8a965a
Gaurav Tomar <gauravtomar977@gmail.com>
2026-08-19 11:40:54 +0530

    Time: 1 ms (32.32%), Space: 8.6 MB (29.09%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -6,16 +6,15 @@ public:
         for (int i = 0; i < n; i++) {
             mp[s[i]]++;
         }
-        string res = "";
-        int seat = 0;
         using p = pair<int, char>;
         priority_queue<p> pq;
-        for (auto a : mp) {
-            pq.push({a.second, a.first});
+        for (auto i : mp) {
+            pq.push({i.second, i.first});
         }
+        int seat = 0;
+        string res = "";
         while (!pq.empty()) {
-            pair<int, char> p1;
-            p1 = pq.top();
+            pair<int, char> p1 = pq.top();
             pq.pop();
             if (seat == 0 || res[seat - 1] != p1.second) {
                 res.push_back(p1.second);
@@ -24,23 +23,23 @@ public:
                 if (p1.first > 0) {
                     pq.push(p1);
                 }
-            } else { /// iska matlb ha jab heap me same frequency ho dono ki
+            } else {
                 if (pq.empty()) {
                     return "";
                 }
-                pair<int, char> p2;
-                p2 = pq.top();
+                pair<int, char> p2 = pq.top();
                 pq.pop();
-                res.push_back(p2.second);
-                seat++;
-                p2.first--;
-                if (p2.first > 0) {
-                    pq.push(p2);
+                if (seat == 0 || res[seat - 1] != p2.second) {
+                    res.push_back(p2.second);
+                    seat++;
+                    p2.first--;
+                    if (p2.first > 0) {
+                        pq.push(p2);
+                    }
+                    pq.push(p1);
                 }
-                pq.push(p1);
             }
         }
         return res;
     }
-
 };
