b7ad31d389c5230835615a450200b63aafc45558
Gaurav Tomar <gauravtomar977@gmail.com>
2026-08-20 11:06:23 +0530

    Time: 10 ms (45.72%), Space: 7.8 MB (65.74%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-    // tabulation with space optimization
     int fib(int n) {
-        if(n<=1) return n;
-        int prev = 1;
-        int prevofPrev = 0;
-        int ans;
-        for (int i = 2; i <= n; i++) {
-            ans = prev + prevofPrev;
-            prevofPrev = prev;
-            prev = ans;
-        }
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        int ans = fib(n - 1) + fib(n - 2);
         return ans;
     }
 };
