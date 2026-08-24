d575a08062b9808e98df6f5fb733a19c3c164443
Gaurav Tomar <gauravtomar977@gmail.com>
2026-08-24 12:17:06 +0530

    Time: 0 ms (100%), Space: 18.6 MB (41.88%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -6,32 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-   bool check(TreeNode*l , TreeNode*r){
-    if(l==NULL && r==NULL){
-        return true;
-    }
-    if(l==NULL || r==NULL){
-        return false;
-    }
-    if(l->val!=r->val){
-        return false;
-    }
-    bool r1 = check(l->left,r->right);
-    bool r2 = check(l->right , r->left);
-    return r1&&r2?true:false;
-
-   }
-    bool isSymmetric(TreeNode* root) {
-        if(root==NULL){
+    bool check(TreeNode* l1, TreeNode* l2) {
+        if (l1 == NULL && l2 == NULL)
             return true;
-        }
-       bool ans =  check(root->left,root->right);
-        return ans;
-        
+        if (l1 == NULL || l2 == NULL)
+            return false;
+        if (l1->val != l2->val)
+            return false;
+        bool r1 = check(l1->left, l2->right);
+        bool r2 = check(l1->right, l2->left);
+        return r1 && r2;
     }
+    bool isSymmetric(TreeNode* root) { return check(root->left, root->right); }
 };
