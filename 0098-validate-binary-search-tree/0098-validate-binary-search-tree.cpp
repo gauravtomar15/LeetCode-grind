2a8bac019db0fe1b5ed31d274cea56ca83f28fbb
Gaurav Tomar <gauravtomar977@gmail.com>
2026-09-02 18:14:50 +0530

    Time: 0 ms (100%), Space: 22 MB (16%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -6,23 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* prev = NULL;
     bool ans = true;
-    void check(TreeNode* root) {
-        if (root == NULL) {
-            return;
+    void check(TreeNode* root){
+        if(root==NULL){
+            return ;
         }
         check(root->left);
-        if (prev == NULL) {
-            prev = root;
-        } else {
-            if (root->val <= prev->val) {
+        if(prev==NULL){
+          prev = root;
+        }else{
+            if(root->val<=prev->val){
                 ans = false;
             }
             prev = root;
