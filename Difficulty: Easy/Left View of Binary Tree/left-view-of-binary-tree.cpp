b70dd69ee6b8fd29dacd4004a3af88f877b9e1a0
Gaurav Tomar <gauravtomar977@gmail.com>
2026-09-07 12:35:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.cpp
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.cpp
@@ -1,44 +1,44 @@
-/*
+/* Structure of Binary Tree Node
 class Node {
-public:
-    int data;
-    Node* left;
-    Node* right;
-
-    Node(int val) {
-        data = val;
-        left = nullptr;
-        right = nullptr;
-    }
-};
-*/
+	public:
+	int data;
+	Node* left;
+	Node* right;
+	
+	Node(int val) {
+		data = val;
+		left = right = nullptr;
+	}
+}; */
 
 class Solution {
-  public:
-    vector<int> leftView(Node *root) {
-        // code here
-           if (root == NULL) return {};
-
-        vector<int> result;
-        queue<Node*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            int size = q.size();
-
-            for (int i = 0; i < size; i++) {
-                Node* curr = q.front();
-                q.pop();
-
-                if (i == 0) {
-                    result.push_back(curr->data);
-                }
-
-                if (curr->left != NULL) q.push(curr->left);
-                if (curr->right != NULL) q.push(curr->right);
-            }
-        }
-
-        return result;
-    }
+	public:
+	vector<int> leftView(Node *root) {
+		// code here
+		if (root == NULL) {
+			return {};
+		}
+		vector<int> ans;
+		queue<Node*> q;
+		q.push(root);
+		while (!q.empty()) {
+			int n = q.size();
+			
+			for (int i = 0 ; i<n; i++) {
+				Node* t = q.front();
+				q.pop();
+				if (i == 0) {
+					ans.push_back(t->data);
+				}
+				if (t->left != NULL) {
+					q.push(t->left);
+				}
+				if (t->right != NULL) {
+					q.push(t->right);
+				}
+			}
+			
+		}
+		return ans;
+	}
 };
